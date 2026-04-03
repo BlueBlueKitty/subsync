d8cd20d6cf6ebd3b86c08c340c25b62fc070c008
Yibo Yuan <2633669459@qq.com>
2026-04-03 15:40:48 +0800

    🐞 fix: 固定python库版本号

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -2,7 +2,7 @@
 set -eu
 
 IMAGE_NAME="${IMAGE_NAME:-bluebluekitty/subsync}"
-VERSION="${VERSION:-v0.1.1}"
+VERSION="${VERSION:-v0.1.2}"
 VERSION_NO_V="${VERSION#v}"
 PLATFORM="${PLATFORM:-linux/amd64}"
 
